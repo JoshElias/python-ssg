971ac4f0c5348db861aff70d9be842c2e99ced3e
Josh Elias <josh@daemontech.tools>
2024-05-08 11:44:17 -0400

    add textnode with tests

---
+++ main.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env zsh
+python src/main.py
---
+++ src/main.py
@@ -0,0 +1,7 @@
+from textnode import TextNode
+
+def main():
+    node = TextNode("Text Node", "bold", "https://www.boot.dev")
+    print(node)
+
+main()
